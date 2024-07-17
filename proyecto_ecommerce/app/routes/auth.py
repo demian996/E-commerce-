98fe4b87209d4400fcd5ff99bc4609a1dc772aef
JohaoCrespo <jdcrespo@uce.edu.ec>
2024-07-17 12:14:40 -0500

    refactor: add validaciones

--- proyecto_ecommerce/app/routes/auth.py
+++ proyecto_ecommerce/app/routes/auth.py
@@ -2,8 +2,14 @@ from flask import Blueprint, jsonify,  render_template, request, redirect, url_f
 from app.dao.mysql_dao import UsuarioDAO
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.factories.dao_factory import DAOFactory
-
+import re
 auth_bp = Blueprint('auth', __name__)
+
+
+def validar_email(email):
+    # @._
+    pattern = r'^[^@]+@[^@]+\.[^@]+$'
+    return re.match(pattern, email) is not None
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -12,6 +18,11 @@ def login():
         email = request.form['email']
         password = request.form['password']
         usuario = UsuarioDAO.get_by_email(email)
+
+        if not validar_email(email):
+            flash('Correo electrónico con formato no válido', 'danger')
+            return redirect(url_for('auth.register'))
+
         if usuario and check_password_hash(usuario.contrasena, password):
             session['user_id'] = usuario.id
             session['user_name'] = usuario.nombre
@@ -26,13 +37,12 @@ def login():
     return render_template('login.html')
 
 
-
-
 @auth_bp.route('/logout')
 def logout():
     session.clear()
     flash('Has cerrado sesión', 'success')
     return redirect(url_for('main.home'))
+
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -40,6 +50,11 @@ def register():
         nombre = request.form['nombre']
         email = request.form['email']
         contrasena = generate_password_hash(request.form['contrasena'])
+
+        if not validar_email(email):
+            flash('Correo electrónico con formato no válido', 'danger')
+            return redirect(url_for('auth.register'))
+
         rol = 'cliente'  # Por defecto, todos los nuevos usuarios son clientes
         usuario = UsuarioDAO.create(nombre, email, contrasena, rol)
         flash('Registro exitoso', 'success')
